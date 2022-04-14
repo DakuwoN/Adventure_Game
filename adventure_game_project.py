51c3a415692c2be3e19abf5aa3ce506731f6c22a
Matthew McCane <MatthewLeeMcCane@gmail.com>
2022-04-14 18:27:03 -0400

    Updates to adventure game

--- adventure_game_project.py
+++ adventure_game_project.py
@@ -8,9 +8,25 @@
 import time
 
 
+armor = []
+
+
 def print_pause(message):
     print(message, flush=True)
     time.sleep(0)
+
+
+def play_again():
+    while True:
+        play_again = input(
+            "Would you like to play again?\n" "Please type: Yes or No\n").lower()
+        if 'yes' in play_again:
+            intro()
+        elif 'no' in play_again:
+            print_pause("Thank you for playing!")
+            exit()
+        else:
+            print_pause("Please enter 'Yes' or 'No'\n")
 
 
 def intro():
@@ -25,42 +41,6 @@ def intro():
     print_pause(
         "To your left, there is an empty tunnel... that you are curious about...\n")
     destination()
-
-
-def tunnel():
-    print_pause("You walk towards the empty tunnel...")
-    print_pause("The tunnel is dark, but you hear a voice..")
-    print_pause("The voice tells you to come inside!")
-    print_pause("There is a blacksmith waiting for you...")
-    print_pause(
-        "He explains that he has been waiting for a brave man to come inside the tunnel...")
-    print_pause(
-        "He has been hiding inside the tunnel from Khodos, while he's been hiding...")
-    print_pause(
-        "The blacksmith has created a special type of body armor called: invincible")
-    print_pause(
-        "He equips you with this armor, in order to defeat Khodos and not take any damage!")
-
-
-def building():
-    print_pause("You listen to the people of Zaire!")
-    print_pause("You approach the building with your Sword of Dominance..")
-    print_pause("Before you get close enough to the building")
-    print_pause("Khodos runs outside the building and attacks you!\n\n")
-    fight_choice()
-
-
-def fight_choice():
-    fight_choice = input(
-        "1. Would you like to fight Kodos with your Sword of Dominance?\n" "2. Would you rather return to the warzone and re-think the situation?\n")
-    if fight_choice == '1':
-        print_pause(
-            "You swing your Sword of Dominance at Khodos twice, dealing damage!")
-        print_pause(
-            "Khodos retaliates with a very strong breath of fire that he sprays at you.")
-        print_pause(
-            "Unfortunately, the fire deals 100 damage to you, and you die.")
-        play_again()
 
 
 def destination():
@@ -78,15 +58,77 @@ def destination():
                 "Sorry, I do not understand your selection.\n (Please enter 1 or 2)\n")
 
 
-def play_again():
+def tunnel():
 
-    play_again = input(
-        "Would you like to play again?\n" "Please type: Yes or No\n").lower()
-    if 'yes' in play_again:
-        intro()
+    if 'invincible' in armor:
+        print_pause(
+            "The blacksmith has already equipped you with the armor!")
+        print_pause("We shall see if Khodos can win the fight now...")
+        print_pause("You return to the warzone of Zaire...")
+        destination()
     else:
-        if 'no' in play_again:
-            print_pause("Thank you for playing!")
+        print_pause("You walk towards the empty tunnel...")
+        print_pause("The tunnel is dark, but you hear a voice..")
+        print_pause("The voice tells you to come inside!")
+        print_pause("There is a blacksmith waiting for you...")
+        print_pause(
+            "He explains that he has been waiting for a brave man to come inside the tunnel...")
+        print_pause(
+            "He has been hiding inside the tunnel from Khodos, while he's been hiding...")
+        print_pause(
+            "The blacksmith has created a special type of body armor called: invincible")
+        print_pause(
+            "He equips you with this armor, in order to defeat Khodos and not take any damage!")
+        armor.append('invincible')
+        print_pause("You return to the warzone in Zaire...")
+
+    destination()
+
+
+def building():
+    print_pause("You listen to the people of Zaire!")
+    print_pause("You approach the building with your Sword of Dominance..")
+    print_pause("Before you get close enough to the building")
+    print_pause("Khodos runs outside the building and attacks you!\n\n")
+    fight_choice()
+
+
+def fight_choice():
+    while True:
+        fight_choice = input(
+            "1. Would you like to fight Kodos with your Sword of Dominance?\n" "2. Would you rather return to the warzone and re-think the situation?\n\n")
+        if fight_choice == '1':
+            if 'invincible' in armor:
+                print_pause(
+                    "You swing your Sword of Dominance at Khodos twice, dealing damage!")
+                print_pause(
+                    "Khodos retaliates with a very strong breath of fire that he sprays at you!")
+                print_pause("The fire does zero damage...")
+                print_pause(
+                    "Your 'invincible' armor has blocked all incoming damage!")
+                print_pause(
+                    "Kodos realizes he has no other choice, but to flee the warzone!")
+                print_pause(
+                    "Congratulations, you are victorious! You have saved the town of Zaire from Khodos!")
+                play_again()
+            else:
+                print_pause(
+                    "You swing your Sword of Dominance at Khodos twice, dealing damage!")
+                print_pause(
+                    "Khodos retaliates with a very strong breath of fire that he sprays at you.")
+                print_pause(
+                    "Unfortunately, the fire deals 100 damage to you, and you die.")
+                play_again()
+        elif fight_choice == '2':
+            print_pause("You run as fast as you can to avoid Khodos...")
+            print_pause(
+                "Khodos laughs at you... and screams 'NO ONE CAN DEFEAT ME!'")
+            print_pause(
+                "You return back to the town of Zaire, to re-think the situation.")
+            destination()
+        else:
+            print_pause(
+                "Sorry, I did not understand you!\n" "Please select a choice: 1 or 2\n")
 
 
 def play_game():
